b6773cf1dc5a719353a9e18d1602d8742962ce43
RavenIV <torrans5@yandex.ru>
2023-11-29 10:54:43 +0300

    Update PostForm

--- blogicum/blog/forms.py
+++ blogicum/blog/forms.py
@@ -1,6 +1,19 @@
 from django import forms
+from django.forms import ModelChoiceField 
 
 from .models import Post, Comment
+
+
+class LocationChoiceField(ModelChoiceField): 
+
+    def label_from_instance(self, object): 
+        return f'{object.name}' 
+
+
+class CategoryChoiceField(ModelChoiceField): 
+
+    def label_from_instance(self, object): 
+        return f'{object.title}' 
 
 
 class PostForm(forms.ModelForm):
@@ -8,6 +21,10 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         exclude = ('author',)
+        field_classes = {
+            'location': LocationChoiceField,
+            'category': CategoryChoiceField,
+        } 
 
 
 class CommentForm(forms.ModelForm):
